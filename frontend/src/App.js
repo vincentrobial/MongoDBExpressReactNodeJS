b9aa412607d9b0a7188aad4d5a60f2be0064f7d2
vincentrobial <vincent.robial@gmail.com>
2025-11-16 22:37:24 +0100

    t

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -22,7 +22,7 @@ function App() {
     <div>
       <nav className="navbar navbar-expand navbar-dark bg-dark">
         <a href="/restaurants" className="navbar-brand">
-          Restaurant Reviews
+          Restaurant Reviews Test
         </a>
         <div className="navbar-nav mr-auto">
           <li className="nav-item">
